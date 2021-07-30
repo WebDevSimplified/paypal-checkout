79dcef863464815c50dbc85988817877eb7eb69d
Web Dev Simplified <webdevsimplified@gmail.com>
2021-07-30 11:02:59 -0500

    Finish Video

--- public/script.js
+++ public/script.js
@@ -28,9 +28,7 @@ paypal
         })
     },
     onApprove: function (data, actions) {
-      return actions.order.capture().then(function (details) {
-        alert("Transaction completed by " + details.payer.name.given_name)
-      })
+      return actions.order.capture()
     },
   })
   .render("#paypal")
